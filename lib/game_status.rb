18e34d410c6c23063c869ca087af88ffd0e8a2c5
Caroline Forrester <carolineforrester22@gmail.com>
2020-08-10 18:12:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,11 +14,34 @@ WIN_COMBINATIONS = [
   [0,3,6],
   [2,5,8]
   ]
-won do
+def won?(board)
   for each WIN_COMBINATIONS
   in WIN_COMBINATIONS
   win_index_1 = 
   win_combination [0]
+  win_index_2 = 
+  win_combination [1]
+  win_index_3 = 
+  win_combination [2]
+  win_index_4 = 
+  win_combination [3]
+  win_index_5 = 
+  win_combination [4]
+  win_index_6 = 
+  win_combination [5]
+  win_index_7 = 
+  win_combination [6]
+  win_index_8 = 
+  win_combination [7]
+  win_index_9 = 
+  win_combination [8]
+  win_index_10 = 
+  win_combination [0]
+  win_index_11 = 
+  win_combination [4]
+  win_index_12 = 
+  win_combination [8]
+  
   
   position_1 = board[win_index_1]
   
